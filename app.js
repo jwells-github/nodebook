373a0e97e90c264da2fdc2a44c90de540a686e9e
EC2 Default User <ec2-user@ip-172-31-24-74.eu-west-2.compute.internal>
2020-05-15 16:27:47 +0000

    add log out

--- app.js
+++ app.js
@@ -99,6 +99,11 @@ app.use(function(req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
